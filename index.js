d2c0ff9ae4d41e57c36b5a052ec35418b65a2176
Artemis Levitt <artemis.levitt@gmail.com>
2024-07-16 18:17:00 +0200

    adding get response with bunch of endpoints

--- index.js
+++ index.js
@@ -2,6 +2,18 @@ import express from "express";
 const app = express();
 const port = 3000;
 
+app.get("/", (req, res) => {
+    res.send("Answer from server to GET.");
+});
+
+app.get("/about", (req, res) => {
+    res.send("<h1>About me.</h1>");
+});
+
+app.get("/contact", (req, res) => {
+    res.send("<h1>Contacts.</h1>");
+});
+
 app.listen(port, () => {
     console.log(`Server running on port ${port}.`);
 });
